50270dd7feeb0e16d114ce54e2896a1a6ca6d82f
Miguel Mendes <miguel.mdebrito@gmail.com>
2016-09-03 19:10:55 -0300

    add two basic tests

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class HomeTest(TestCase):
+
+    def setUp(self):
+        """setUp get response"""
+        self.response = self.client.get('/')
+
+    def test_get(self):
+        """Test successful response code"""
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        """Test expected template used"""
+        self.assertTemplateUsed(self.response, 'index.html')
